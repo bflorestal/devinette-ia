91fa394625b4c57af8c5a35be228eaaa52920a9a
bflorestal <yayou7710@hotmail.fr>
2024-07-21 00:58:45 +0200

    feat: added header and footer

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,18 +1,26 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+// import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { ModeToggle } from "@/components/mode-toggle";
+import GitHubIcon from "@/components/icons/github";
+import GamepadIcon from "@/components/icons/gamepad";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <header className="sticky top-0 z-30 w-full border-b border-gray-700/80 shadow-sm">
-        <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-          <Link
-            to="/"
-            className="[&.active]:font-bold transition-opacity hover:opacity-90 text-lg"
-          >
-            Accueil
-          </Link>
+      <header className="px-4 lg:px-6 h-14 flex items-center">
+        <Link to="/" className="flex items-center justify-center">
+          <GamepadIcon className="h-6 w-6" />
+          <span className="sr-only">Devinette IA</span>
+        </Link>
+        <div className="ml-auto flex gap-4 sm:gap-6 items-center">
+          <nav className="flex gap-4 sm:gap-6">
+            <Link
+              to="#"
+              className="text-sm font-medium hover:underline underline-offset-4"
+            >
+              Statistiques
+            </Link>
+          </nav>
           <div className="flex flex-1 items-center justify-end space-x-4">
             <ModeToggle />
           </div>
@@ -21,7 +29,24 @@ export const Route = createRootRoute({
       <main className="flex-1">
         <Outlet />
       </main>
-      <TanStackRouterDevtools />
+      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+        <p className="text-xs text-muted-foreground">
+          &copy; {new Date().getFullYear()} Bryan Florestal, Tous droits
+          réservés.
+        </p>
+        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          <a
+            href="https://github.com/bflorestal/devinette-ia"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-flex items-center gap-2 text-xs hover:underline underline-offset-4"
+          >
+            <GitHubIcon className="h-4 w-4" />
+            GitHub
+          </a>
+        </nav>
+      </footer>
+      {/* <TanStackRouterDevtools /> */}
     </>
   ),
 });
