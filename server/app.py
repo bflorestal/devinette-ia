7615210678363c92684d78479f186633442952e7
bflorestal <yayou7710@hotmail.fr>
2024-07-21 15:46:38 +0200

    chore: added CORS support

--- server/app.py
+++ server/app.py
@@ -2,7 +2,11 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
     return {"message": "Hello, World!"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
