4a0146d58d146b95f610aa172dc8fdd215069de0
bflorestal <yayou7710@hotmail.fr>
2024-07-30 21:13:44 +0200

    chore: disabling router devtools if not DEV

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -8,16 +8,16 @@ import GamepadIcon from "@/components/icons/gamepad";
 export const Route = createRootRoute({
   component: () => (
     <>
-      <header className="px-4 lg:px-6 h-14 flex items-center">
+      <header className="flex h-14 items-center px-4 lg:px-6">
         <Link to="/" className="flex items-center justify-center">
           <GamepadIcon className="h-6 w-6" />
           <span className="sr-only">Devinette IA</span>
         </Link>
-        <div className="ml-auto flex gap-4 sm:gap-6 items-center">
+        <div className="ml-auto flex items-center gap-4 sm:gap-6">
           <nav className="flex gap-4 sm:gap-6">
             <Link
               to="/stats"
-              className="text-sm font-medium hover:underline underline-offset-4"
+              className="text-sm font-medium underline-offset-4 hover:underline"
             >
               Statistiques
             </Link>
@@ -30,17 +30,17 @@ export const Route = createRootRoute({
       <main className="flex-1">
         <Outlet />
       </main>
-      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+      <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-6">
         <p className="text-xs text-muted-foreground">
           &copy; {new Date().getFullYear()} Bryan Florestal, Tous droits
           réservés.
         </p>
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+        <nav className="flex gap-4 sm:ml-auto sm:gap-6">
           <a
             href="https://github.com/bflorestal/devinette-ia"
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-flex items-center gap-2 text-xs hover:underline underline-offset-4"
+            className="inline-flex items-center gap-2 text-xs underline-offset-4 hover:underline"
           >
             <GitHubIcon className="h-4 w-4" />
             GitHub
@@ -48,7 +48,7 @@ export const Route = createRootRoute({
         </nav>
       </footer>
       <Toaster richColors />
-      <TanStackRouterDevtools />
+      {import.meta.env.DEV && <TanStackRouterDevtools />}
     </>
   ),
 });
