78fc580b98b16e8a5668524b85f59d0a3d471130
bflorestal <yayou7710@hotmail.fr>
2024-07-30 21:45:45 +0200

    chore: changed bg for key features text

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -53,7 +53,7 @@ function Index() {
         <div className="container px-4 md:px-6">
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-2">
-              <div className="inline-block rounded-lg bg-muted px-3 py-1 text-sm">
+              <div className="inline-block rounded-lg bg-background px-3 py-1 text-sm">
                 Fonctionnalités clés
               </div>
               <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
