360ad9983d3211098267e1248822b38e1dab74ba
bflorestal <yayou7710@hotmail.fr>
2024-07-31 03:09:11 +0200

    feat: added not found component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { Link, RouterProvider, createRouter } from "@tanstack/react-router";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./index.css";
 
@@ -9,7 +9,34 @@ import "./index.css";
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  defaultNotFoundComponent: () => {
+    return (
+      <section className="w-full py-12 md:py-24 lg:py-32">
+        <div className="space-y-4">
+          <h1 className="text-center text-3xl font-bold sm:text-5xl xl:text-6xl/none">
+            404
+          </h1>
+
+          <div className="mx-auto flex max-w-2xl flex-col items-center justify-between space-y-6">
+            <p className="text-center text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+              La page que vous recherchez peut avoir été supprimée, avoir changé
+              de nom ou être temporairement indisponible.
+            </p>
+
+            <Link
+              to="/"
+              className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+            >
+              Retour à l'accueil
+            </Link>
+          </div>
+        </div>
+      </section>
+    );
+  },
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
